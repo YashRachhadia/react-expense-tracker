e3974dcdb9fc4f56db860f2e6078867e4372fa27
Yash Rachhadia <yashrachhadia7844@gmail.com>
2021-09-05 18:24:10 +0530

    complete expense tracker

---
+++ src/context/GlobalState.js
@@ -0,0 +1,42 @@
+import React, { createContext, useReducer } from "react";
+import GlobalReducer from "./GlobalReducer";
+
+const initialState = {
+  transactions: JSON.parse(localStorage.getItem("transactions")),
+};
+
+//Create Context
+export const GlobalContext = createContext(initialState);
+
+//Provider
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(GlobalReducer, initialState);
+  const { Provider } = GlobalContext;
+
+  //Actions
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  };
+
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  };
+
+  return (
+    <Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </Provider>
+  );
+};
